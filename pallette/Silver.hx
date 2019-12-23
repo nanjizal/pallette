3d5339ad7f513ca3cdec68a2ee62dfcba1175a49
nanjizal <nanjizal@users.noreply.github.com>
2019-12-23 14:22:09 +0000

    add Silver pallette

---
+++ pallette/Silver.hx
@@ -0,0 +1,20 @@
+package pallette;
+
+abstract enum AllSilvers( Int ) to Int from Int {
+    var platinum  = 0xffE5E4E2;
+    var lavenderTint = 0xffDADADA;
+    var lightSilver = 0xffD8D8D8;
+    var paleSilver  = 0xffC9C0BB;
+    var silverPink = 0xffC4AEAD;
+    var silver = 0xffC0C0C0;
+    var silverSand = 0xffBFC1C2;
+    var silverChalice = 0xffACACAC;
+    var quicksilver = 0xffA6A6A6;
+    var lightSirocco  = 0xffB8C2C2;
+    var newSilver = 0xffBFB8A5;
+    var romanSilver = 0xff838996;
+    var sirocco = 0xff18080;
+    var oldSilver = 0xff848482;
+    var sonicSilver  = 0xff757575;
+    var darkSilver = 0xff71706E; 
+}
