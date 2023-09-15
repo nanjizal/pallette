f57d1bcf5a4ae2dc81051107e95333a341a153b1
nanjizal <nanjizal@users.noreply.github.com>
2023-09-15 04:12:00 +0100

    Create SRLCH2.hx

---
+++ src/pallette/utils/SRLCH2.hx
@@ -0,0 +1,13 @@
+package pallette.utils;
+import pallette.utils.LCH;
+/** 
+   { l, c, h, alpha }
+   , L – perceived lightness, c – radius, h – angle, alpha -alpha
+**/
+@:forward
+abstract SRLCH2( LCH ) from LCH to LCH {
+    public inline
+    function new( L: Float, c: Float, h: Float, alpha: Float ){
+        this = new LCH( L, c, h, alpha ); 
+    }
+}
