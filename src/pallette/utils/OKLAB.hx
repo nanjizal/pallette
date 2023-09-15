fb61bf5fe84b0329677b4a98dab989a7e9e013fb
nanjizal <nanjizal@users.noreply.github.com>
2023-09-15 03:26:21 +0100

    Update OKLAB.hx
    change to abstract LAB

--- src/pallette/utils/OKLAB.hx
+++ src/pallette/utils/OKLAB.hx
@@ -1,18 +1,13 @@
 package pallette.utils;
+import pallette.utils.LAB;
 /** 
    { l, a, b, alpha }
    , L – perceived lightness, a – how green/red the color is, b – how blue/yellow the color is, alpha -alpha
 **/
-@:structInit
-class OKLAB {
-    public var L = 0.; 
-    public var a = 0.;
-    public var b = 0.;
-    public var alpha = 0.;
-    public function new( L: Float, a: Float, b: Float, alpha: Float ){
-        this.L = L; 
-        this.a = a;
-        this.b = b;
-        this.alpha = alpha;
-    }
+@:forward
+abstract OKLAB( LAB ) from LAB to LAB {
+   public inline
+   function new( L: Float, a: Float, b: Float, alpha: Float ){
+      this = new LAB( L, a, b, alpha );
+   }
 }
