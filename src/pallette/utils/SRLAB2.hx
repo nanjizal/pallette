3cb24f1e23b83ef9b436c124c4c162e0eb38d009
nanjizal <nanjizal@users.noreply.github.com>
2023-09-15 03:27:25 +0100

    Create SRLAB2.hx
    add SRLab2

---
+++ src/pallette/utils/SRLAB2.hx
@@ -0,0 +1,13 @@
+package pallette.utils;
+import pallette.utils.LAB;
+/** 
+   { l, a, b, alpha }
+   , L – perceived lightness, a – how green/red the color is, b – how blue/yellow the color is, alpha -alpha
+**/
+@:forward
+abstract SRLAB2( LAB ) from LAB to LAB {
+   public inline
+   function new( L: Float, a: Float, b: Float, alpha: Float ){
+      this = new LAB( L, a, b, alpha );
+   }
+}
