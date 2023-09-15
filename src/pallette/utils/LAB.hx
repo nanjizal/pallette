a7a27d8e06f6ad2040f1ba8fbc69226d450b5b9a
nanjizal <nanjizal@users.noreply.github.com>
2023-09-15 03:22:06 +0100

    Create LAB.hx
    add LAB structInit

---
+++ src/pallette/utils/LAB.hx
@@ -0,0 +1,18 @@
+package pallette.utils;
+/** 
+   { l, a, b, alpha }
+   , L – perceived lightness, a – how green/red the color is, b – how blue/yellow the color is, alpha -alpha
+**/
+@:structInit
+class LAB {
+    public var L = 0.; 
+    public var a = 0.;
+    public var b = 0.;
+    public var alpha = 0.;
+    public function new( L: Float, a: Float, b: Float, alpha: Float ){
+        this.L = L; 
+        this.a = a;
+        this.b = b;
+        this.alpha = alpha;
+    }
+}
