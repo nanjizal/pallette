0c8a9c924620337687800127a3c99cdd194627ef
nanjizal <nanjizal@users.noreply.github.com>
2023-09-15 04:11:05 +0100

    change OKLCH.hx to abstract type

--- src/pallette/utils/OKLCH.hx
+++ src/pallette/utils/OKLCH.hx
@@ -1,19 +1,13 @@
 package pallette.utils;
-// initial commit.
+import pallette.utils.LCH;
 /** 
    { l, c, h, alpha }
-   , L – perceived lightness, c – , h – , alpha -alpha
+   , L – perceived lightness, c – radius, h – angle, alpha -alpha
 **/
-@:structInit
-class OKLCH {
-    public var L = 0.; 
-    public var c = 0.;
-    public var h = 0.;
-    public var alpha = 0.;
-    public function new( L: Float, c: Float, h: Float, alpha: Float ){
-        this.L = L; 
-        this.c = c;
-        this.h = h;
-        this.alpha = alpha;
+@:forward
+abstract OKLCH( LCH ) from LCH to LCH {
+    public inline
+    function new( L: Float, c: Float, h: Float, alpha: Float ){
+        this = new LCH( L, c, h, alpha ); 
     }
 }
